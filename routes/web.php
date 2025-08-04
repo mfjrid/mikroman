994de5d277fff99efa8107996555e793fe3b5116
Muhammad Fajar <mfjr48@gmail.com>
2025-08-04 20:22:24 +0700

    add: mikrotik user route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\MikrotikController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MikrotikController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\MikrotikUserController;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
@@ -11,3 +12,14 @@ Route::post('/mikrotiks/{mikrotik}/test-connection', [MikrotikController::class,
 
 Route::get('/monitor/{mikrotik}', [DashboardController::class, 'monitor'])->name('monitor');
 Route::get('/api/stats/{mikrotik}', [DashboardController::class, 'getStats'])->name('api.stats');
+
+// User Management Routes
+Route::prefix('mikrotik/{mikrotik}/users')->group(function () {
+    Route::get('/', [MikrotikUserController::class, 'index'])->name('mikrotik-users.index');
+    Route::get('/create', [MikrotikUserController::class, 'create'])->name('mikrotik-users.create');
+    Route::post('/', [MikrotikUserController::class, 'store'])->name('mikrotik-users.store');
+    Route::get('/{userId}/edit', [MikrotikUserController::class, 'edit'])->name('mikrotik-users.edit');
+    Route::put('/{userId}', [MikrotikUserController::class, 'update'])->name('mikrotik-users.update');
+    Route::delete('/{userId}', [MikrotikUserController::class, 'destroy'])->name('mikrotik-users.destroy');
+    Route::post('/{userId}/toggle-status', [MikrotikUserController::class, 'toggleStatus'])->name('mikrotik-users.toggle-status');
+});
