8a9e84ed15fb3e48349977d649c181ebb9ce2bc7
Muhammad Fajar <mfjr48@gmail.com>
2025-08-07 21:15:51 +0700

    add: PPP route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MikrotikController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\MikrotikPPPController;
 use App\Http\Controllers\MikrotikUserController;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
@@ -22,4 +23,16 @@ Route::prefix('mikrotik/{mikrotik}/users')->group(function () {
     Route::put('/{userId}', [MikrotikUserController::class, 'update'])->name('mikrotik-users.update');
     Route::delete('/{userId}', [MikrotikUserController::class, 'destroy'])->name('mikrotik-users.destroy');
     Route::post('/{userId}/toggle-status', [MikrotikUserController::class, 'toggleStatus'])->name('mikrotik-users.toggle-status');
+});
+
+// PPP Secret Management Routes
+Route::prefix('mikrotik/{mikrotik}/ppp-secrets')->group(function () {
+    Route::get('/', [MikrotikPPPController::class, 'index'])->name('mikrotik-ppp.index');
+    Route::get('/create', [MikrotikPPPController::class, 'create'])->name('mikrotik-ppp.create');
+    Route::post('/', [MikrotikPPPController::class, 'store'])->name('mikrotik-ppp.store');
+    Route::get('/{secretId}/edit', [MikrotikPPPController::class, 'edit'])->name('mikrotik-ppp.edit');
+    Route::put('/{secretId}', [MikrotikPPPController::class, 'update'])->name('mikrotik-ppp.update');
+    Route::delete('/{secretId}', [MikrotikPPPController::class, 'destroy'])->name('mikrotik-ppp.destroy');
+    Route::post('/{activeId}/disconnect', [MikrotikPPPController::class, 'disconnect'])->name('mikrotik-ppp.disconnect');
+    Route::get('/refresh-status', [MikrotikPPPController::class, 'refreshStatus'])->name('mikrotik-ppp.refresh');
 });
